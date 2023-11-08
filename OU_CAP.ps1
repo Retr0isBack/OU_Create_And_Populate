70a14a88df75fcb29f5571f091cd142142a1002c
Daniel Dreher <73075247+Retr0isBack@users.noreply.github.com>
2023-11-08 09:46:52 +0100

    Add OU Generation and Variable Changes

--- OU_CAP.ps1
+++ OU_CAP.ps1
@@ -1,5 +1,9 @@
 # Import active directory module for running AD cmdlets
 Import-Module ActiveDirectory
+
+# Create an OU subfolder when transferring userdate from an old OU to a new OU 
+$NewOU = #For Example the old OU name + Users so for ex. StuttgartUsers or TimbuktuUsers
+New-ADOrganizationalUnit -Name $NewOU -Path "DC=XYZ,DC=XYZ"
   
 # Store the data from NewUsersFinal.csv in the $ADUsers variable
 $ADUsers = Import-Csv "XYZ:\xyz\xyz.csv" #-Delimiter ";"
@@ -10,13 +14,12 @@ $UPN = "exoip.com"
 # Loop through each row containing user details in the CSV file
 foreach ($User in $ADUsers) {
 
-    #Read user data from each field in each row and assign the data to a variable as below
+    # Read user data from each field in each row and assign the data to a variable as below
     $username = $User.username
-    $password = $User.password #This line can obviously also be used to give all new users one premade PW
+    $password = $User.password # This line can obviously also be used to give all new users one premade PW
     $firstname = $User.firstname
     $lastname = $User.lastname
     $initials = $User.initials
-    $OU = $User.ou #This field refers to the OU the user account is to be created in
     $email = $User.email
     $streetaddress = $User.streetaddress
     $city = $User.city
@@ -32,7 +35,7 @@ foreach ($User in $ADUsers) {
     if (Get-ADUser -Filter "SamAccountName -eq '$username'") {
         
         # If user does exist, give a warning
-        Write-Warning "A user account with username $username already exists in Active Directory."
+        Write-Warning "A user account with username $username already exists in this Organizational Unit."
     }
     else {
 
@@ -47,7 +50,7 @@ foreach ($User in $ADUsers) {
             -Initials $initials `
             -Enabled $True `
             -DisplayName "$firstname $lastname" `
-            -Path $OU `
+            -Path $NewOU `
             -City $city `
             -PostalCode $zipcode `
             -Country $country `
